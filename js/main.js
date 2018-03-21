ad41c8794d1581dc53692a4042c4027a10c5916a
Ding <l_ding5@tss-hr420-dm08.fc.ca>
2018-03-21 15:37:45 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -2,6 +2,39 @@
 
 // JavaScript Document
 
-(() ==>
-console.log('video stuff');
+(() => {
+console.log('video stuff fired!');
+
+ const vidPlayer = document.querySelector('video'),
+ playButtom =document.querySelector('button')[0],
+  pauseButtom =document.querySelector('button')[1],
+  rewindButtom =document.querySelector('button')[2],
+
+
+  function volOn() {
+
+    vidPlayer.muted = false;
+}
+  function volOff() {
+    vidPlayer.muted = true;
+  }
+  function rewindVid(){
+    vidPlayer.currentTime -= 5;
+  }
+  function playVideo(){
+    vidPlayer.play();
+  }
+  function pauseVideo(){
+    vidPlayer.pause();
+  }
+
+
+vidPlayer.addEventListener('moseover' volOn);
+vidPlayer.addEventListener('moseout' volOff);
+
+rewindButtom.addEventListener('click',rewindVid);
+pauseButtom.addEventListener('click',pauseVid);
+playButtom.addEventListener('click',playVid)
+
+
 })();
